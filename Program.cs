c562fdeffc93115b94418c786d70600c50e9913e
rafael <rafael201123r@gmail.com>
2024-09-02 09:02:04 -0300

    calculadora de expressão pelo terminal

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using NCalc;
+
+namespace CalculadoraTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool escolha = true;
+
+            while (escolha)
+            {
+                Console.WriteLine("Digite sua expressão matemática ou sair):");
+                string expressao = Console.ReadLine();
+
+                if (expressao.ToLower() == "sair")
+                {
+                    escolha = false;
+                    Console.WriteLine("Programa Finalizado");
+                }
+                else
+                {
+                        Expression exp = new Expression(expressao);
+                        object resultado = exp.Evaluate();
+                        Console.WriteLine("Resultado da expressão: " + resultado);
+                }
+            }
+        }
+    }
+}
